c107493c685e8f8a373766ce1f28a1148343c50c
Vinibfx <116393152+Vinibfx@users.noreply.github.com>
2022-11-03 13:22:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import pandas as pd
 
 from sklearn import datasets 
 
-from sklearn.naive_bayes import GaussianNB
-
+from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
 **Flor Iris **
